a84cfe773f2233f4655ab8ed090b41438dd37156
theod07 <theodore.do@gmail.com>
2016-09-21 17:52:58 -0700

    using sample template from https://github.com/appium/sample-code

--- tests/test_rename.py
+++ tests/test_rename.py
@@ -4,14 +4,15 @@ Create a simple test that will open Appium Webdriver and visit www.google.com
 
 # iOS environment
 from appium import webdriver
-# from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-# from unittest.Testcase import assertEqual
-import time
+from time import sleep
+import unittest
 import os
+
 import ipdb as pdb
 
-"""*** WebDriverException: Message: An unknown server-side error occurred 
+"""
+*** WebDriverException: Message: An unknown server-side error occurred 
 while processing the command. Original error: Could not find a device to 
 launch. You requested 'iPhone 5s (7.1 Simulator)', but the available 
 devices were: [
@@ -26,115 +27,141 @@ devices were: [
 		"iPhone 6 (8.3 Simulator) [452532BA-AD63-4312-8288-C107242186A3]",
 		"iPhone 6 Plus (8.3 Simulator) [163386AB-E6BE-4DC2-96F0-A9494E34427D]"
 	]
-
 """
 
-{
-  'platformName': 'iOS',
-  'platformVersion': '7.1',
-  'browserName': 'Safari',
-  'deviceName': 'iPhone Simulator'
-}
+class WebViewIOSTests(unittest.TestCase):
 
-# python
-# setup the web driver and launch the webview app.
-desired_caps = {}
-desired_caps['platformName'] = 'iOS'
-desired_caps['platformVersion'] = '8.3'
-desired_caps['deviceName'] = 'iPhone Simulator'
-pdb.set_trace()
+	def setUp(self):
+		# set up appium
+		desired_caps = {}
+		desired_caps['platformName'] = 'iOS'
+		desired_caps['platformVersion'] = '8.3'
+		desired_caps['deviceName'] = 'iPhone Simulator'
 
-driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
-driver.implicitly_wait(3) # seconds
+		self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+		self.driver.implicitly_wait(3)
 
-# Navigate to the page and interact with the elements on the guinea-pig page using id.
-driver.get('http://saucelabs.com/test/guinea-pig')
-
-# navigate to solvhealth.com
-driver.get("http://www.solvhealth.com")
-driver.switch_to_alert().accept()
-
-# enter credentials
-alert = driver.switch_to_alert()
-alert.send_keys('quick' + Keys.RETURN + 'solv' + Keys.RETURN)
-
-# page 1, click arrow
-# time.sleep(1.5)
-elem = driver.find_element_by_class_name('_34eC')
-elem.click()
-
-# page 2, click arrow
-# time.sleep(1.5)
-elem = driver.find_element_by_class_name('_34eC')
-elem.click()
-
-# click button "Set my location" 
-# time.sleep(1.5)
-elem = driver.find_element_by_css_selector('input[value="Set my location"]')
-# elem = driver.find_element_by_class_name('_3y2m')
-elem.click()
+	def tearDown(self):
+		self.driver.quit()
 
 
-# allow location
-alert = driver.switch_to_alert()
-alert.accept()
-alert = driver.switch_to_alert()
-alert.accept()
+	def example_test_get_url(self):
+		self.driver.get('http://www.google.com')
+		# self.driver.switch_to.context('WEBVIEW')
 
-# select text box & enter text
-# time.sleep(1.5)
-elem = driver.find_element_by_class_name('geosuggest__input')
-elem.send_keys('Dallas, TX' + Keys.ENTER)
+		search = self.driver.find_element_by_class_name('gsfi')
+		search.send_keys('sauce labs')
+		search.send_keys(Keys.RETURN)
 
-# enter symptoms
-elem = driver.find_element_by_id('symptoms')
-elem.send_keys('back pain' + Keys.ENTER)
+		# allow the page to load
+		sleep(1)
+
+		self.assertEquals('sauce labs - Google Search', self.driver.title)
 
 
-# click "book"
-# time.sleep(5)
-driver.find_element_by_link_text('Book').click()
 
-# click "change"
+# # python
+# # setup the web driver and launch the webview app.
+# desired_caps = {}
+# desired_caps['platformName'] = 'iOS'
+# desired_caps['platformVersion'] = '8.3'
+# desired_caps['deviceName'] = 'iPhone Simulator'
+# pdb.set_trace()
 
-# click "next"
-# time.sleep(1.5)
-driver.find_element_by_class_name('_2u1X').click()
+# driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+# driver.implicitly_wait(3) # seconds
 
-# fill form
-elem = driver.find_element_by_id('firstName')
-elem.send_keys('theo')
-elem = driver.find_element_by_id('lastName')
-elem.send_keys('do')
-elem = driver.find_element_by_id('email')
-elem.send_keys('theo@solvhealth.com')
-elem = driver.find_element_by_id('phone')
-elem.send_keys('7148236827')
-elem = driver.find_element_by_id('notes')
-elem.send_keys('testing')
-# time.sleep(1.5)
-elem = driver.find_element_by_class_name('_1RVK')
-elem.click()
+# # Navigate to the page and interact with the elements on the guinea-pig page using id.
+# driver.get('http://saucelabs.com/test/guinea-pig')
 
-# flow branches here for insurance
-# No. I know I'm covered or will check at clinic
-elem = driver.find_element_by_class_name('_2fkl')
-elem.click()
+# # navigate to solvhealth.com
+# driver.get("http://www.solvhealth.com")
+# driver.switch_to_alert().accept()
 
-# click finalize
-# time.sleep(1.5)
-elem = driver.find_element_by_class_name('_1lJn')
-elem.click()
+# # enter credentials
+# alert = driver.switch_to_alert()
+# alert.send_keys('quick' + Keys.RETURN + 'solv' + Keys.RETURN)
 
-pdb.set_trace()
+# # page 1, click arrow
+# # sleep(1.5)
+# elem = driver.find_element_by_class_name('_34eC')
+# elem.click()
 
-# div = driver.find_element_by_id('i_am_an_id')
-# check the text retrieved matches expected value
-# assertEqual('I am a div', div.text)
+# # page 2, click arrow
+# # sleep(1.5)
+# elem = driver.find_element_by_class_name('_34eC')
+# elem.click()
 
-# populate the comments field by id
-# driver.find_element_by_id('comments').send_keys('My comment')
+# # click button "Set my location" 
+# # sleep(1.5)
+# elem = driver.find_element_by_css_selector('input[value="Set my location"]')
+# # elem = driver.find_element_by_class_name('_3y2m')
+# elem.click()
 
-# close the driver
-driver.quit()
 
+# # allow location
+# alert = driver.switch_to_alert()
+# alert.accept()
+# alert = driver.switch_to_alert()
+# alert.accept()
+
+# # select text box & enter text
+# # sleep(1.5)
+# elem = driver.find_element_by_class_name('geosuggest__input')
+# elem.send_keys('Dallas, TX' + Keys.ENTER)
+
+# # enter symptoms
+# elem = driver.find_element_by_id('symptoms')
+# elem.send_keys('back pain' + Keys.ENTER)
+
+
+# # click "book"
+# # sleep(5)
+# driver.find_element_by_link_text('Book').click()
+
+# # click "change"
+
+# # click "next"
+# # sleep(1.5)
+# driver.find_element_by_class_name('_2u1X').click()
+
+# # fill form
+# elem = driver.find_element_by_id('firstName')
+# elem.send_keys('theo')
+# elem = driver.find_element_by_id('lastName')
+# elem.send_keys('do')
+# elem = driver.find_element_by_id('email')
+# elem.send_keys('theo@solvhealth.com')
+# elem = driver.find_element_by_id('phone')
+# elem.send_keys('7148236827')
+# elem = driver.find_element_by_id('notes')
+# elem.send_keys('testing')
+# # sleep(1.5)
+# elem = driver.find_element_by_class_name('_1RVK')
+# elem.click()
+
+# # flow branches here for insurance
+# # No. I know I'm covered or will check at clinic
+# elem = driver.find_element_by_class_name('_2fkl')
+# elem.click()
+
+# # click finalize
+# # sleep(1.5)
+# elem = driver.find_element_by_class_name('_1lJn')
+# elem.click()
+
+# pdb.set_trace()
+
+# # div = driver.find_element_by_id('i_am_an_id')
+# # check the text retrieved matches expected value
+# # assertEqual('I am a div', div.text)
+
+# # populate the comments field by id
+# # driver.find_element_by_id('comments').send_keys('My comment')
+
+# # close the driver
+# driver.quit()
+
+if __name__ == '__main__':
+	suite = unittest.TestLoader().loadTestsFromTestCase(WebViewIOSTests)
+	unittest.TextTestRunner(verbosity=2).run(suite)
